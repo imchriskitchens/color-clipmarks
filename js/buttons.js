aa5666bfdf99ddd70f4d6e90a3ebffb338fc6c06
Christopher Kitchens <37012798+imchriskitchens@users.noreply.github.com>
2019-01-18 13:16:56 -0500

    Added color sort
    The function takes the data from textarea, strips the '#' prefix, and creates a data array. Duplicate values are then removed and the data array is sorted in alphabetical order. 
    
    Next, we loop through the array to create a list and prefix each value with a '#' and set the value of the textarea. (This should probably be an array map instead)
    
    Next, we loop through the array to create the box previews using the div template and style them by calling setColors(). In retrospect, this function may not be necessary and should probably be removed in a future update.

--- js/buttons.js
+++ js/buttons.js
@@ -36,7 +36,164 @@ function reset() {
 	textAreaStyle();
 	inputStyle();
 	submit();
+}let dataArray;
+const divtemp = `<div style='background-color: #fff;' onmousedown='bgChange(this.style.backgroundColor)' class="box"></div>`;
+
+// -------------------------------------------------------------- SUBMIT
+function submit() {
+	let divList = '';
+	let data = txtareaCol[0].value;
+
+	let prefix = /#/g;
+	dataArray = data.replace(prefix, '').split('\n');
+
+	for (i = 0; i < dataArray.length; i++) {
+		divList += divtemp;
+	}
+
+	colorContainer.innerHTML = divList;
+	setColors();
+}
+
+// -------------------------------------------------------------- SET_COLORS
+function setColors() {
+	for (i = 0; i < dataArray.length; i++) {
+		boxes[i].style.color = '#fff';
+		boxes[i].style.backgroundColor = '#' + dataArray[i];
+		boxes[i].style.margin = '5px';
+		boxes[i].style.float = 'left';
+		boxes[i].style.width = '10vw';
+		boxes[i].style.height = '20vh';
+		boxes[i].style.borderRadius = '5px';
+		boxes[i].style.textAlign = 'center';
+	}
+}
+
+// -------------------------------------------------------------- RESET
+function reset() {
+	textAreaStyle();
+	inputStyle();
+	submit();
+}
+
+// -------------------------------------------------------------- SORT
+let sorted, colors;
+
+function sortColors() {
+	sorted = dataArray.sort();
+	return sorted;
+}
+
+function rmDuplicates() {
+	let unique = {};
+	colors.forEach(function(i) {
+		if (!unique[i]) {
+			unique[i] = true;
+		}
+	});
+	return Object.keys(unique);
+}
+
+function sortAll() {
+	let divList = '';
+	let newList = '';
+	let data = txtareaCol[0].value;
+
+	let prefix = /#/g;
+	dataArray = data.replace(prefix, '').split('\n');
+
+	colors = dataArray;
+	dataArray = rmDuplicates();
+	dataArray = sortColors();
+
+	for (i = 0; i < dataArray.length; i++) {
+		let newLine = `#${dataArray[i]}\n`;
+		newList += newLine;
+	}
+	txtareaCol[0].value = newList;
+
+	for (i = 0; i < dataArray.length; i++) {
+		divList += divtemp;
+	}
+
+	colorContainer.innerHTML = divList;
+	setColors();
 }
 
 // -------------------------------------------------------------- SORT
 // function sort() {}
+let dataArray;
+const divtemp = `<div style='background-color: #fff;' onmousedown='bgChange(this.style.backgroundColor)' class="box"></div>`;
+
+// -------------------------------------------------------------- SUBMIT
+function submit() {
+	let divList = '';
+	let data = txtareaCol[0].value;
+
+	let prefix = /#/g;
+	dataArray = data.replace(prefix, '').split('\n');
+
+	for (i = 0; i < dataArray.length; i++) {
+		divList += divtemp;
+	}
+
+	colorContainer.innerHTML = divList;
+	setColors();
+}
+
+// -------------------------------------------------------------- SET_COLORS
+function setColors() {
+	for (i = 0; i < dataArray.length; i++) {
+		boxes[i].style.color = '#fff';
+		boxes[i].style.backgroundColor = '#' + dataArray[i];
+		boxes[i].style.margin = '5px';
+		boxes[i].style.float = 'left';
+		boxes[i].style.width = '10vw';
+		boxes[i].style.height = '20vh';
+		boxes[i].style.borderRadius = '5px';
+		boxes[i].style.textAlign = 'center';
+	}
+}
+
+// -------------------------------------------------------------- RESET
+function reset() {
+	textAreaStyle();
+	inputStyle();
+	submit();
+}
+
+// -------------------------------------------------------------- SORT
+function rmDuplicates() {
+	let unique = {};
+	dataArray.forEach(function(i) {
+		if (!unique[i]) {
+			unique[i] = true;
+		}
+	});
+	return Object.keys(unique);
+}
+
+function sortAll() {
+	let divList = '';
+	let newList = '';
+	let data = txtareaCol[0].value;
+
+	let prefix = /#/g;
+	dataArray = data.replace(prefix, '').split('\n');
+
+	dataArray = rmDuplicates();
+	dataArray = dataArray.sort();
+
+	for (i = 0; i < dataArray.length; i++) {
+		let newLine = `#${dataArray[i]}\n`;
+		newList += newLine;
+	}
+	txtareaCol[0].value = newList;
+
+	for (i = 0; i < dataArray.length; i++) {
+		divList += divtemp;
+	}
+
+	colorContainer.innerHTML = divList;
+	setColors();
+}
