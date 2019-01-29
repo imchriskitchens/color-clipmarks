6dc442c67c9044e0e443eaad55062a201e860bb4
Christopher Kitchens <37012798+imchriskitchens@users.noreply.github.com>
2019-01-29 18:25:20 -0500

    Update styles.js

--- js/styles.js
+++ js/styles.js
@@ -5,15 +5,17 @@ const bod = document.body,
 	boxes = document.getElementsByClassName('box'),
 	inputBox = document.getElementById('inputBox'),
 	inputDiv = document.getElementById('inputDiv'),
+	noteSecDiv = document.getElementById('noteDiv'),
+	noteSecHeader = document.getElementById('notesHd'),
+	noteSec = document.getElementsByClassName('notes'),
 	txtareaCol = document.getElementsByTagName('textarea'),
 	colorContainer = document.getElementById('colorContainer');
-
 
 
 // ----------------------------------------------------------------- BODY
 function bodyStyle() {
 	bod.style.fontFamily = 'Arial';
-	bod.style.backgroundColor = '#f2f2f2';
+	bod.style.backgroundColor = '#f4f4f4';
 	bod.style.margin = '20px 10% 150px 10%';
 }
 bodyStyle();
@@ -75,11 +77,12 @@ function textAreaStyle() {
 	let thisWidth = 100 - totalMargins;
 	thisWidth += '%';
 
-	txtareaCol[0].value = `#339966\n#339999\n#3399cc\n#3399ff\n#33cc00\n#33cc33\n#33cc66\n#33cc99\n#33cccc\n#33ccff\n#33ff00\n#33ff33\n#33ff66\n#33ff99\n#ff0000\n#ff0033\n#ff0066\n#ff0099\n#ff00cc\n#ff00ff\n#ff3300\n#ff3333\n#ff3366\n#ff3399\n#ff33cc\n#ff33ff\n#ff6600\n#ff6633\n#ff6666\n#ff6699\n#ff66cc\n#ff66ff\n#ff9900\n#ff9933\n#ff9966\n#ff9999\n#ff99cc\n#ff99ff\n#ffcc00\n#ffcc33\n#ffcc66\n#ffcc99\n#ffcccc\n#ffccff\n#ffff00\n#ffff33\n#ffff66\n#ffff99\n#ffffff`;
+	txtareaCol[0].value = `#ff0000\n#ff0033\n#ff0066\n#ff0099\n#ff00cc\n#ff00ff\n#ff3300\n#ff3333\n#ff3366\n#ff3399\n#ff33cc\n#ff33ff\n#ff6600\n#ff6633\n#ff6666\n#ff6699\n#ff66cc\n#ff66ff\n#ff9900\n#ff9933\n#ff9966\n#ff9999\n#ff99cc\n#ff99ff\n#ffcc00\n#ffcc33\n#ffcc66\n#ffcc99`;
 
 	for (i = 0; i < txtareaCol.length; i++) {
 		txtareaCol[i].style.fontSize = '18px';
 		txtareaCol[i].style.color = '#444';
+		txtareaCol[i].style.backgroundColor = '#fafafa';
 		txtareaCol[i].style.width = thisWidth;
 		txtareaCol[i].style.height = '200px';
 		txtareaCol[i].style.paddingTop = '20px';
@@ -87,13 +90,14 @@ function textAreaStyle() {
 		txtareaCol[i].style.marginLeft = margins;
 		txtareaCol[i].style.marginTop = '10%';
 		txtareaCol[i].style.marginRight = margins;
+		txtareaCol[i].style.border = 'solid 1px #ccc';
 		txtareaCol[i].style.borderRadius = '5px';
 		txtareaCol[i].rows = 8;
 	}
 }
 textAreaStyle();
 
-// ----------------------------------------------------------------- BUTTON
+// ----------------------------------------------------------------- BUTTONS
 function buttonStyle() {
 	for (i = 0; i < btns.length; i++) {
 		btns[i].style.fontSize = '20px';
@@ -102,16 +106,43 @@ function buttonStyle() {
 		btns[i].style.color = 'white';
 		btns[i].style.backgroundColor = '#555';
 		btns[i].style.margin = '50px 50px 50px 0';
-		btns[i].style.border = 'none';
+		btns[i].style.border = 'solid 2px #444';
 		btns[i].style.borderRadius = '5px';
 	}
 }
 buttonStyle();
 
+function menuButtonStyle() {
+	// btns[0].style.display = 'block';
+	btns[0].style.display = 'none';
+	btns[0].style.float = 'right';
+	btns[0].style.marginTop = '0';
+	btns[0].style.backgroundColor = '#ccc';
+	btns[0].style.border = 'solid 2px #cbcbcb';
+}
+menuButtonStyle();
+
+function hexToRgbStyle() {
+	btns[3].style.backgroundColor = '#0c2941'; 
+	btns[3].style.border = 'solid 2px #0d395d';
+}
+hexToRgbStyle();
+
+function cutPasteStyle() {
+	btns[5].style.backgroundColor = '#ccc';
+	btns[5].style.border = 'solid 2px #cbcbcb';
+	btns[6].style.backgroundColor = '#ccc';
+	btns[6].style.border = 'solid 2px #cbcbcb';
+
+	btns[5].style.color = '#ccc';
+	btns[6].style.color = '#ccc';
+}
+cutPasteStyle();
+
 // ----------------------------------------------------------------- INPUT_DIV
 function inputDivStyle() {
-	inputDiv.style.width = '80vw';
-	inputDiv.style.backgroundColor = '#f2f2f2';
+	inputDiv.style.width = '85vw';
+	inputDiv.style.backgroundColor = '#f4f4f4';
 	inputDiv.style.borderBottom = 'solid 1px #ccc';
 	inputDiv.style.top = '0';
 	inputDiv.style.position = 'sticky';
@@ -123,6 +154,7 @@ inputDivStyle();
 function inputStyle() {
 	inputBox.value = '#C010R';
 	inputBox.style.fontSize = '20px';
+	inputBox.style.backgroundColor = '#fafafa';
 	inputBox.style.color = '#444';
 	inputBox.style.width = '30vw';
 	inputBox.style.margin = '10px 0 20px 0';
@@ -136,3 +168,23 @@ function inputStyle() {
 	inputBox.style.borderLeft = lrBorder;
 }
 inputStyle();
+
+function bgColor3(num) {
+	let el = num;
+	btns[el].style.background = '#333';
+}
+
+function bgColor5(num) {
+	let el = num;
+	btns[el].style.background = '#555';
+}
+function bgColorMenu3(num) {
+	let el = num;
+	btns[el].style.background = '#aaa';
+}
+
+function bgColorMenu5(num) {
+	let el = num;
+	btns[el].style.background = '#ccc';
+}
+
