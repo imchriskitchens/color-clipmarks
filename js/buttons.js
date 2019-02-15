b75c0c3d5ece159e1b8b363e6e7177aa8b1803e9
Christopher Kitchens <37012798+imchriskitchens@users.noreply.github.com>
2019-02-15 06:23:08 -0500

    Update buttons.js
    Added export function

--- js/buttons.js
+++ js/buttons.js
@@ -103,3 +103,20 @@ function darkMode() {
     bod.style.backgroundColor = bkgdSet;
     colorLabelDiv.style.backgroundColor = bkgdSet;
 }
+
+
+// -------------------------------------------------------------- EXPORT
+function download(filename, text) {
+    let element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+}
+document.getElementById("btnEXPORT").addEventListener("click", function() {
+    let text = document.getElementById("userInput").value;
+    let filename = "colors.txt";
+    download(filename, text);
+}, false);
