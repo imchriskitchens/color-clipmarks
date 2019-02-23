140aef409b2d6ba52a4295d63abe1dfff6132abc
Christopher Kitchens <37012798+imchriskitchens@users.noreply.github.com>
2019-02-23 03:36:41 -0500

    Update stateCtrl.js

--- js/stateCtrl.js
+++ js/stateCtrl.js
@@ -1,78 +1,73 @@
 const btnHex = document.getElementById('btnHEX'),
     btnRgb = document.getElementById('btnRGB'),
+    btnHsl = document.getElementById('btnHSL'),
     btnSort = document.getElementById('btnSORT'),
     btnSubmit = document.getElementById('btnSUBMIT'),
     btnSelect = document.getElementById('btnSELECT'),
-    btnClear = document.getElementById('btnCLEAR');
-
-function hexModeSel() {
-    btnRgb.disabled = false;
-    btnSort.disabled = false;
-    btnHex.disabled = true;
-    upcase.disabled = false;
-    btnRgb.style.color = `#ccc`;
-    btnSort.style.color = `#ccc`;
-    btnHex.style.color = `#ccc`;
-    upcase.style.color = `#ccc`;
-    btnRgb.style.backgroundColor = `#222`;
-    btnSort.style.backgroundColor = `#222`;
-    btnHex.style.backgroundColor = `#222`;
-    upcase.style.backgroundColor = `#222`;
-    btnHex.style.borderColor = '#00bcd4';
-    btnRgb.style.borderColor = '#333';
-    btnHsl.innerHTML = 'HSL > HEX';
-}
-
-function rgbModeSel() {
-    upperCase = false;
-    btnRgb.disabled = true;
-    btnSort.disabled = true;
-    btnHex.disabled = false;
-    upcase.disabled = true;
-    btnSort.style.color = `#666`;
-    btnHex.style.color = `#ccc`;
-    upcase.style.color = `#666`;
-    btnSort.style.backgroundColor = `#1b1b1b`;
-    btnHex.style.backgroundColor = `#222`;
-    upcase.style.backgroundColor = `#1b1b1b`;
-    btnRgb.style.borderColor = '#00bcd4';
-    btnHex.style.borderColor = '#333';
-    upcase.style.borderColor = '#333';
-    btnHsl.innerHTML = 'HSL > RGB';
-}
+    btnClear = document.getElementById('btnCLEAR'),
+    btnExport = document.getElementById('btnEXPORT');
 
 function isClear() {
+    let btnArray = [btnSelect, btnClear, btnHsl, btnExport];
+    for (i = 0; i < btnArray.length; i++) {
+        btnArray[i].disabled = true;
+        btnArray[i].classList.toggle("btn-disabled");
+    }
+    if (upperCase === true) {
+        toggleCase();
+    }
     btnRgb.disabled = true;
     btnHex.disabled = true;
-    btnSort.disabled = true;
-    btnSelect.disabled = true;
-    btnClear.disabled = true;
-    upcase.disabled = true;
-    btnRgb.style.color = `#666`;
-    btnHex.style.color = `#666`;
-    btnSort.style.color = `#666`;
-    btnSelect.style.color = `#666`;
-    btnClear.style.color = `#666`;
-    upcase.style.color = `#666`;
-    btnRgb.style.backgroundColor = `#1b1b1b`;
-    btnHex.style.backgroundColor = `#1b1b1b`;
-    btnSort.style.backgroundColor = `#1b1b1b`;
-    btnSelect.style.backgroundColor = `#1b1b1b`;
-    btnClear.style.backgroundColor = `#1b1b1b`;
-    upcase.style.backgroundColor = `#1b1b1b`;
 }
 
 function notCleared() {
-    hexModeSel();
-    btnSelect.disabled = false;
-    btnClear.disabled = false;
-    upcase.disabled = false;
-    btnSort.style.color = `#ccc`;
-    btnSelect.style.color = `#ccc`;
-    btnClear.style.color = `#ccc`;
-    upcase.style.color = `#ccc`;
-    btnSort.style.backgroundColor = `#222`;
-    btnSelect.style.backgroundColor = `#222`;
-    btnClear.style.backgroundColor = `#222`;
-    upcase.style.backgroundColor = `#222`;
+    let btnArray = [btnSelect, btnClear, btnHsl, btnExport];
+    for (i = 0; i < btnArray.length; i++) {
+        btnArray[i].disabled = false;
+        btnArray[i].classList.toggle("btn-disabled");
+    }
+    btnRgb.disabled = false;
+    btnHex.disabled = true;
 }
+
+function toggleHighlight() {
+    let current = colorMode;
+    switch (current) {
+        case 'hex':
+            btnHex.classList.toggle("btn-highlight");
+            btnHsl.classList.toggle("btn-disabled");
+            btnSort.classList.toggle("btn-disabled");
+            upcase.classList.toggle("btn-disabled");
+            break;
+        case 'rgb':
+            btnRgb.classList.toggle("btn-highlight");
+            break;
+        case 'hsl':
+            btnHsl.classList.toggle("btn-highlight");
+            btnHex.classList.toggle("btn-disabled");
+            break;
+    }
+}
+
+function cMode(mode) {
+    toggleHighlight();
+    colorMode = mode;
+    toggleHighlight();
+    return colorMode;
+}
+
+// function lightModeVars() {
+//     colorDef = '#fff';
+//     bgDef = '#555';
+//     colorDis = '#ccc';
+//     bgDis = '#999';
+//     modeSel = '#0094d4';
+// }
+// 
+// function darkModeVars() {
+//     colorDef = '#ccc';
+//     bgDef = '#222';
+//     colorDis = '#666';
+//     bgDis = '#1b1b1b';
+//     modeSel = '#00bcd4';
+// }
