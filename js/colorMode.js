23cf3fdd9521fcc3a1a793e4939ba3838cd862da
Christopher Kitchens <37012798+imchriskitchens@users.noreply.github.com>
2019-03-31 12:35:46 -0400

    Update colorMode.js

--- js/colorMode.js
+++ js/colorMode.js
@@ -1,115 +1,58 @@
-let r, g, b, data;
-
-function rgbMode() {
-    if (colorMode !== 'rgb') {
-        let newList = '',
-            data = userInput.value;
-
-        if (colorMode === 'hex') {
-            let prefix = /#/g,
-                dataArray = data.replace(prefix, '').split('\n');
-
-            for (i = 0; i < dataArray.length; i++) {
-                let newColorRGB = hexToRGB(dataArray[i]),
-                    result = `rgb(${r}, ${g}, ${b})`;
-                newList = (i === 0) ? newList += result : newList += `\n${result}`;
-            }
-            userInput.value = newList;
-
-            let cLabel = hexToRGB(colorLabel.value),
-                cLabelNew = `rgb(${r}, ${g}, ${b})`;
-            colorLabel.value = '';
-        }
-        else {
-            let data = userInput.value;
-            if (data !== '') {
-                dataArray = data.split('\n');
-                let qty = dataArray.length;
-                for (i = 0; i < qty; i++) {
-                    newList += template;
-                }
-                colorContainer.innerHTML = newList;
-                setColors();
-            }
-            newList = '';
-            for (i = 0; i < boxes.length; i++) {
-                let result = boxes[i].style.backgroundColor;
-                newList = (i === 0) ? newList += result : newList += `\n${result}`;
-            }
-            userInput.value = newList;
-            colorLabel.value = '';
-        }
-        cMode('rgb');
-    }
-}
-
 function hexToRGB(hex) {
-    if (hex.length === 6) {
-        r = parseInt(hex.substring(0, 2), 16);
-        g = parseInt(hex.substring(2, 4), 16);
-        b = parseInt(hex.substring(4, 6), 16);
-        return {
-            r,
-            g,
-            b
-        };
-    } else if (hex.length === 3) {
-        r = parseInt(hex[0] + hex[0], 16);
-        g = parseInt(hex[1] + hex[1], 16);
-        b = parseInt(hex[2] + hex[2], 16);
-        return {
-            r,
-            g,
-            b
-        };
-    }
+    hex = hex.charAt(0) === "#" ? hex.slice(1) : hex;
+    const bigint = parseInt(hex, 16);
+    let r = (bigint >> 16) & 255,
+        g = (bigint >> 8) & 255,
+        b = bigint & 255;
+    return `rgb(${r}, ${g}, ${b})`;
 }
 
-function hexMode() {
-    if (colorMode !== 'hex') {
-        let newList = '';
-        let data = userInput.value;
-        let dataArray = data.split('\n');
-        for (i = 0; i < dataArray.length; i++) {
-            let result = (dataArray[i].charAt(0) === 'r') ? rgbToHex(dataArray[i]) : '';
-            newList = (i === 0) ? newList += result : newList += `\n${result}`;
-        }
-        userInput.value = newList;
-        colorLabel.value = '';
-        cMode('hex');
+function hexToHSL(hex) {
+    let r = 0,
+        g = 0,
+        b = 0;
+    if (hex.length == 4) {
+        r = "0x" + hex[1] + hex[1];
+        g = "0x" + hex[2] + hex[2];
+        b = "0x" + hex[3] + hex[3];
+    } else if (hex.length == 7) {
+        r = "0x" + hex[1] + hex[2];
+        g = "0x" + hex[3] + hex[4];
+        b = "0x" + hex[5] + hex[6];
     }
+    r /= 255;
+    g /= 255;
+    b /= 255;
+    let cmin = Math.min(r, g, b),
+        cmax = Math.max(r, g, b),
+        delta = cmax - cmin,
+        h = 0,
+        s = 0,
+        l = 0;
+    if (delta == 0) h = 0;
+    else if (cmax == r) h = ((g - b) / delta) % 6;
+    else if (cmax == g) h = (b - r) / delta + 2;
+    else h = (r - g) / delta + 4;
+    h = Math.round(h * 60);
+    if (h < 0) h += 360;
+    l = (cmax + cmin) / 2;
+    s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+    s = +(s * 100).toFixed(0);
+    l = +(l * 100).toFixed(0);
+    return `hsl(${h}, ${s}%, ${l}%)`;
 }
 
-function hslConvert() {
-    let data = userInput.value;
-    if (data.charAt(0) === 'h') {
-        submit();
-        let newList = '';
-        for (i = 0; i < boxes.length; i++) {
-            let c = boxes[i].style.backgroundColor;
-            c = (btnHex.disabled === true) ? rgbToHex(c) : c;
-            newList = (i === 0) ? newList += `${c}` : newList += `\n${c}`;
-        }
-        userInput.value = newList;
-    }
-    else {
-        alert('Currently, the first value must be HSL format to convert values.');
-    }
-}
-
-function hslMode() {
-    if (colorMode !== 'hsl') {
-        let newList = '',
-            data = userInput.value,
-            dataArray = data.split('\n');
-        for (i = 0; i < dataArray.length; i++) {
-            let result = (dataArray[i].charAt(0) === 'r') ? dataArray[i] : '';
-            result = rgbToHsl(result);
-            newList = (i === 0) ? newList += result : newList += `\n${result}`;
-        }
-        userInput.value = newList;
-        colorLabel.value = '';
-        cMode('hsl');
+function rgbToHEX(val) {
+    if (val.charAt(0) == 'r') {
+        val = val.replace('rgb(', '').replace(')', '').split(',');
+        let r = parseInt(val[0], 10).toString(16),
+            g = parseInt(val[1], 10).toString(16),
+            b = parseInt(val[2], 10).toString(16);
+        r = r.length == 1 ? '0' + r : r;
+        g = g.length == 1 ? '0' + g : g;
+        b = b.length == 1 ? '0' + b : b;
+        let result = '#' + r + g + b;
+        return result;
     }
 }
 
@@ -147,4 +90,91 @@ function rgbToHSL(val) {
         let result = `hsl(${h.toFixed(0)}, ${s.toFixed(0)}%, ${l.toFixed(0)}%)`;
         return result;
     }
+}
+
+function hslToRGB(hsl) {
+    hsl = hsl.replace(/%/g, "").match(/\w+/g);
+    h = hsl[1];
+    s = hsl[2] / 100;
+    l = hsl[3] / 100;
+    let c = (1 - Math.abs(2 * l - 1)) * s,
+        x = c * (1 - Math.abs((h / 60) % 2 - 1)),
+        m = l - c / 2,
+        r = 0,
+        g = 0,
+        b = 0;
+    if (0 <= h && h < 60) {
+        r = c;
+        g = x;
+        b = 0;
+    } else if (60 <= h && h < 120) {
+        r = x;
+        g = c;
+        b = 0;
+    } else if (120 <= h && h < 180) {
+        r = 0;
+        g = c;
+        b = x;
+    } else if (180 <= h && h < 240) {
+        r = 0;
+        g = x;
+        b = c;
+    } else if (240 <= h && h < 300) {
+        r = x;
+        g = 0;
+        b = c;
+    } else if (300 <= h && h < 360) {
+        r = c;
+        g = 0;
+        b = x;
+    }
+    r = Math.round(Math.abs((r + m) * 255));
+    g = Math.round(Math.abs((g + m) * 255));
+    b = Math.round(Math.abs((b + m) * 255));
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+function hslToHEX(hsl) {
+    hsl = hsl.replace(/%/g, "").match(/\w+/g);
+    h = hsl[1];
+    s = hsl[2] / 100;
+    l = hsl[3] / 100;
+    let c = (1 - Math.abs(2 * l - 1)) * s,
+        x = c * (1 - Math.abs((h / 60) % 2 - 1)),
+        m = l - c / 2,
+        r = 0,
+        g = 0,
+        b = 0;
+    if (0 <= h && h < 60) {
+        r = c;
+        g = x;
+        b = 0;
+    } else if (60 <= h && h < 120) {
+        r = x;
+        g = c;
+        b = 0;
+    } else if (120 <= h && h < 180) {
+        r = 0;
+        g = c;
+        b = x;
+    } else if (180 <= h && h < 240) {
+        r = 0;
+        g = x;
+        b = c;
+    } else if (240 <= h && h < 300) {
+        r = x;
+        g = 0;
+        b = c;
+    } else if (300 <= h && h < 360) {
+        r = c;
+        g = 0;
+        b = x;
+    }
+    r = Math.round((r + m) * 255).toString(16);
+    g = Math.round((g + m) * 255).toString(16);
+    b = Math.round((b + m) * 255).toString(16);
+    if (r.length == 1) r = "0" + r;
+    if (g.length == 1) g = "0" + g;
+    if (b.length == 1) b = "0" + b;
+    return `#${r}${g}${b}`;
 }
