fb5ebf8d8707a0a119aad2f64bfeee8b61b9111b
Christopher Kitchens <37012798+imchriskitchens@users.noreply.github.com>
2019-08-30 08:01:17 -0400

    Add files via upload
    updated content filtering

--- js/file-upload.js
+++ js/file-upload.js
@@ -2,13 +2,18 @@ const contentFilter = (content) => {
   const isHEX = /(^#?[0-9A-F]{6}$)|(^#?[0-9A-F]{3}$)/i;
   const isRGB = /^rgb\((\s*\d{1,3}\s*),(\s*\d{1,3}\s*),(\s*\d{1,3}\s*)\)$/i;
   const isHSL = /^hsl\((\s*\d{1,3}\s*),(\s*\d{1,3}%\s*),(\s*\d{1,3}%\s*)\)$/i;
+  const testArray = [["#", isHEX], ["rgb", isRGB], ["hsl", isHSL]];
 
-  const lines = content.trim().split(`\n`);
-  lines.forEach(color => {
-    userInput.value +=
-      (isHEX.test(color)) ? `${color}\n` :
-      (isRGB.test(color)) ? `${color}\n` :
-      (isHSL.test(color)) ? `${color}\n` : '';
+  const updateList = (val) => userInput.value += `${val}\n`;
+
+  const lines = content.replace(/[:;"']/g, "").trim().split(`\n`);
+  lines.forEach(line => {
+    let segment = "";
+    testArray.forEach(([item, isValid] = flag) => {
+      segment = line.replace(item, `\n${item}`).split(/\n/)[1];
+      segment = segment.replace(/([,]\s+)/g, ",").split(/\s+/g)[0];
+      if (isValid.test(segment)) updateList(segment.match(isValid)[0]);
+    });
   });
   submit();
 }
@@ -18,7 +23,7 @@ const pullfiles = function () {
   const file = fileInput.files[0];
   const reader = new FileReader();
   reader.onload = function (evt) {
-    userInput.value = '';
+    userInput.value = "";
     contentFilter(evt.target.result);
   };
   reader.readAsText(file);
