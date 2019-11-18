6568e941ed7c67a03f86e9e19f0a086f77f98037
Christopher Kitchens <37012798+imchriskitchens@users.noreply.github.com>
2019-11-17 21:12:10 -0500

    Add files via upload

--- js/conversions.js
+++ js/conversions.js
@@ -1,78 +1,79 @@
-const to16 = (n) => parseInt(n, 10).toString(16);
-const expand = (n) => (n.length === 1) ? `0${n}` : n;
+const hexString = ({ r, g, b }) => `#${r}${g}${b}` /**/ .toUpperCase() /**/ ;
+const rgbString = ({ r, g, b }) => `rgb(${r}, ${g}, ${b})`;
+const hslString = ({ h, s, l }) => `hsl(${h}, ${s}%, ${l}%)`;
 
-const digits = (str) => str.match(/\d+/g);
-const minMax = (r, g, b) => [Math.min(r, g, b), Math.max(r, g, b)];
+/* data flag: allows functions to return data as an array */
 
-const rgbToHEX = (rgb) => {
-  let [r, g, b] = digits(rgb);
-  [r, g, b] = [r, g, b].map(to16);
-  [r, g, b] = [r, g, b].map(expand);
-  return `#${r}${g}${b}`;
+const rgbToHEX = (value, { data } = false) => {
+  let [r, g, b] = value.match(/\d+/g);
+  [r, g, b] = [r, g, b].map((n) => parseInt(n, 10).toString(16));
+  [r, g, b] = [r, g, b].map((n) => n.length === 1 ? `0${n}` : n);
+  return (data) ? ([r, g, b]) : hexString({ r, g, b });
 }
 
-const rgbToHSL = (rgb) => {
-  let [r, g, b] = digits(rgb).map((n) => n / 255);
-  let [min, max] = minMax(r, g, b);
-  let [hue, sat, lit, c] = Array(4).fill(0);
+const rgbToHSL = (value, { data } = false) => {
+  let [r, g, b] = (Array.isArray(value)) ? value: value.match(/\d+/g);
+  [r, g, b] = [r, g, b].map((v) => v / 255);
 
-  lit = (max + min) / 2;
-  c = max - min;
+  let cmin = Math.min(r, g, b);
+  let cmax = Math.max(r, g, b);
+  let c = cmax - cmin;
 
-  if (c !== 0) hue = (max === r) ? ((g - b) / c) % 6 :
-    (max === g) ? (b - r) / c + 2 :
-    (r - g) / c + 4; //(max === b)
+  let [h, s, l] = [0, 0, (cmax + cmin) * 0.5];
 
-  hue *= 60;
-  if (hue < 0) hue += 360;
-  if (c !== 0) sat = (c) / (1 - Math.abs(max + min - 1));
-  sat *= 100;
-  lit *= 100;
+  if(c !== 0) {
+    h = // condition hue value
+      (cmax === r) ? ((g - b) / c) % 6 :
+      (cmax === g) ? (b - r) / c + 2 :
+      (r - g) / c + 4; // (cmax === b)
 
-  [hue, sat, lit] = [hue, sat, lit].map((n) => parseInt(n));
-  return `hsl(${hue}, ${sat}%, ${lit}%)`;
+    s = c / (1 - Math.abs(cmax + cmin - 1));
+  }
+
+  [h, s, l] = [h * 60, s * 100, l * 100];
+  if(h < 0) h += 360; // neg hue correction
+  [h, s, l] = [h, s, l].map((n) => parseInt(n));
+
+  return (data) ? ([h, s, l]) : hslString({ h, s, l });
 }
 
-const hexToRGB = (hex) => {
-  hex = +`0x${hex.replace("#", "")}`;
-  let [r, g, b] = [(hex >> 16), (hex >> 8), hex].map((n) => n & 255);
-  return `rgb(${r}, ${g}, ${b})`;
+const hslToRGB = (value, { data } = false) => {
+  let [h, s, l] = value.match(/\d+/g);
+  [h, s, l] = [h / 60, s / 100, l / 100];
+
+  let c = s * (1 - Math.abs(2 * l - 1));
+  let x = c * (1 - Math.abs(h % 2 - 1));
+  let m = l - c / 2;
+
+  [c, x, m] = [(c + m), (x + m), m].map((v) => Math.round(v * 255));
+  [c, x, m] = [c, x, m].map((v) => (v < 1) ? 0 : v);
+
+  let [r, g, b] = [
+    [c, x, m],
+    [x, c, m],
+    [m, c, x],
+    [m, x, c],
+    [x, m, c],
+    [c, m, x]
+  ][Math.floor(h) % 6];
+
+  return (data) ? ([r, g, b]) : rgbString({ r, g, b });
 }
 
-const hexToHSL = (hex) => {
-  hex = hexToRGB(hex);
-  return rgbToHSL(hex);
+const hslToHEX = (value, { data } = false) => {
+  let [r, g, b] = hslToRGB(value, { data: true });
+  [r, g, b] = [r, g, b].map((n) => parseInt(n, 10).toString(16));
+  [r, g, b] = [r, g, b].map((n) => n.length === 1 ? `0${n}` : n);
+  return (data) ? ([r, g, b]) : hexString({ r, g, b });
 }
 
-function hslToHEX(hsl) {
-  let [h, s, l] = digits(hsl);
-  s /= 100;
-  l /= 100;
-
-  let [c, x, m, r, g, b] = Array(6).fill(0);
-  c = (1 - Math.abs(2 * l - 1)) * s;
-  x = c * (1 - Math.abs((h / 60) % 2 - 1));
-  m = l - c / 2;
-
-  const sixty = (h) => Math.floor(h / 60) % 6;
-
-  [r, g, b] =
-  (sixty(h) == 0) ? [c, x, 0] :
-  (sixty(h) == 1) ? [x, c, 0] :
-  (sixty(h) == 2) ? [0, c, x] :
-  (sixty(h) == 3) ? [0, x, c] :
-  (sixty(h) == 4) ? [x, 0, c] :
-  (sixty(h) == 5) ? [c, 0, x] : [r, g, b];
-
-  r = Math.round((r + m) * 255).toString(16);
-  g = Math.round((g + m) * 255).toString(16);
-  b = Math.round((b + m) * 255).toString(16);
-
-  [r, g, b] = [r, g, b].map(expand);
-  return `#${r}${g}${b}`;
+const hexToRGB = (value, { data } = false) => {
+  const hex = `0x${value.replace("#", "")}`;
+  const [r, g, b] = [(hex >> 16), (hex >> 8), hex].map((n) => n & 255);
+  return (data) ? ([r, g, b]) : rgbString({ r, g, b });
 }
 
-const hslToRGB = (hsl) => {
-  let hex = hslToHEX(hsl);
-  return hexToRGB(hex);
+const hexToHSL = (value) => {
+  const [r, g, b] = hexToRGB(value, { data: true });
+  return rgbToHSL([r, g, b]);
 }
